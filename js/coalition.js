e64236deaa59e22685522de8fd4e7022be268f5c
Chris Antaki <ChrisAntaki@gmail.com>
2015-06-10 00:04:36 -0700

    Wired up hero form

--- js/coalition.js
+++ js/coalition.js
@@ -1,12 +1,116 @@
-// Check for outdated browsers
-var isIE = navigator.userAgent.match(/MSIE (\d+)\./);
-if (isIE) {
-    var version = +isIE[1];
-    if (version < 10) {
-        alert('Unfortunately your browser, Internet Explorer ' + version + ', is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+// Check for outdated browsers.
+(function() {
+    var isIE = navigator.userAgent.match(/MSIE (\d+)\./);
+    if (isIE) {
+        var version = +isIE[1];
+        if (version < 10) {
+            alert('Unfortunately your browser, Internet Explorer ' + version + ', is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+        }
     }
+
+    if (navigator.userAgent.match(/Android 2\.3/)) {
+        alert('Unfortunately your browser, Android 2.3, is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+    }
+})();
+
+
+
+// Setup shortcuts for AJAX.
+var ajax = {
+    get: function(url, callback) {
+        callback = callback || function() {};
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                callback(xhr.response);
+            }
+        };
+        xhr.open('get', url, true);
+        xhr.send();
+    },
+
+    post: function(url, formData, callback) {
+        callback = callback || function() {};
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                callback(xhr.response);
+            }
+        };
+        xhr.open('post', url, true);
+        xhr.send(formData);
+    },
+};
+
+
+
+// Application state.
+var state = {
+    savedEmail: '',
+};
+
+
+
+// Start application.
+(function() {
+    setupHeroForm();
+
+    loadCategories();
+
+    loadArtistsFromDB({
+        category: null,
+        page: 1,
+        size: 10,
+    });
+})();
+
+
+
+function loadArtistsFromDB(params) {
+    var url =
+        'https://coalition-api.herokuapp.com/artists/' +
+        params.size + '/' +
+        params.page + '/' +
+        (params.category || '');
+
+    ajax.get(url, function(res) {
+        console.log('Get artists from DB');
+        console.log(JSON.parse(res));
+    });
 }
 
-if (navigator.userAgent.match(/Android 2\.3/)) {
-    alert('Unfortunately your browser, Android 2.3, is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+function loadCategories() {
+    ajax.get('https://coalition-api.herokuapp.com/categories', function(res) {
+        console.log('Get categories');
+        console.log(JSON.parse(res));
+    });
+}
+
+function setupHeroForm() {
+    document.getElementById('hero-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        var tag = 'artistscoalition';
+
+        var data = new FormData();
+        data.append('guard', '');
+        data.append('hp_enabled', true);
+        data.append('tag', tag);
+        data.append('org', 'fftf');
+
+        var emailElement = document.getElementById('hero-form-email');
+        var email = emailElement.value;
+        if (!email) {
+            alert('Please enter your email.');
+            return emailElement.focus();
+        }
+
+        data.append('member[email]', email);
+
+        // ajax.post('https://queue.fightforthefuture.org/action', data);
+
+        document.querySelector('header .email').className += ' thanks ';
+    });
 }
